d6b1b4342a6e6d48e5b30d5f76505f41153833c9
VictorRValencia <rayovalenciav@gmail.com>
2022-12-11 14:58:25 -0500

    Implementacion Create.sql, log4j

---
+++ create.sql
@@ -0,0 +1,3 @@
+create table IF NOT EXISTS address (id int auto_increment primary key,street varchar(255),location varchar (255),province varchar (255));
+create table IF NOT EXISTS patients(id int auto_increment primary key,firstName varchar(255),lastName varchar (255),dni varchar (255),dateOfAdmission TIMESTAMP WITHOUT TIME ZONE,domicilio_id int);
+create table IF NOT EXISTS dentists(id int auto_increment primary key,firstName varchar(255),lastName varchar (255),tuition int);

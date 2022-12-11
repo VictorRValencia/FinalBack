bbd568ba331c70bdcd6dcda57024ec64f0ceef23
VictorRValencia <rayovalenciav@gmail.com>
2022-12-11 14:33:50 -0500

    Subiendo mi proyecto a git

---
+++ src/main/java/com/company/ProyectoFinalBack/ProyectoFinalBackApplication.java
@@ -0,0 +1,13 @@
+package com.company.ProyectoFinalBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/ProyectoFinalBack/ProyectoFinalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ProyectoFinalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
